afa8006a8eaceb3ea4066d7c553a670f60b1816c
Andy Pike <andy@andypike.com>
2016-09-07 23:59:17 +0100

    Initial commit after moving apps from rumbl project

---
+++ apps/rumbl/config/dev.secrets.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :info_sys, :wolfram, app_id: "9E2282-TG3L8EQUXG"
---
+++ apps/rumbl/lib/rumbl.ex
@@ -0,0 +1,35 @@
+defmodule Rumbl do
+
+  @moduledoc """
+  Main application configuration
+  """
+
+  use Application
+  alias Rumbl.Endpoint, as: Endpoint
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(Rumbl.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(Rumbl.Endpoint, []),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
